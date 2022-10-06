092b2613e66bae78123ea1d77a3b9ad094457ae5
akutruff <874049+akutruff@users.noreply.github.com>
2022-10-06 14:09:29 +0000

    add email to privacy policy

--- apps/pricey/pages/privacy.tsx
+++ apps/pricey/pages/privacy.tsx
@@ -112,6 +112,13 @@ const PrivacyPolicy: NextPage = ({ }) => {
       <p>Another part of our priority is adding protection for children while using the internet. We encourage parents and guardians to observe, participate in, and/or monitor and guide their online activity.</p>
 
       <p>Pricey does not knowingly collect any Personal Identifiable Information from children under the age of 13. If you think that your child provided this kind of information on our website, we strongly encourage you to contact us immediately and we will do our best efforts to promptly remove such information from our records.</p>
+
+      <h2>Contact us</h2>
+
+      <p>You may contact us about this policy at kutruffllc@proton.me</p>
+
+      <p>Pricey does not knowingly collect any Personal Identifiable Information from children under the age of 13. If you think that your child provided this kind of information on our website, we strongly encourage you to contact us immediately and we will do our best efforts to promptly remove such information from our records.</p>
+
     </div>
   );
 };
