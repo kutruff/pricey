e7fda2be601c2b97ebadb184396e39e994a46c78
akutruff <874049+akutruff@users.noreply.github.com>
2022-10-07 20:22:42 +0000

    add sitemap generation

---
+++ apps/pricey/next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: process.env.WEBSITE_URL || 'https://example.com',
+    generateRobotsTxt: true, // (optional)
+    changefreq: 'hourly',
+    // ...other options
+}
